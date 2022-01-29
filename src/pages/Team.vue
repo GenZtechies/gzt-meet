782b2ac0c6e8028986042047ead3ed4a7aec0a5f
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-01-29 01:37:30 +0100

    feat: homepage and team added

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
-        <h1>The team</h1>
+        <section class="flex flex-col justify-center min-h-full py-10 md:py-0">
+        <h1 class="text-5xl md:text-6xl text-center text-gray-900 font-semibold">The team!</h1>
+
+        <div class="flex flex-wrap justify-evenly items-center w-3/4 m-auto py-5">
+            <div class="bg-indigo-800 w-56 h-56 lg:w-64 lg:h-64 mx-auto my-4 rounded-full"></div>
+            <div class="bg-indigo-800 w-56 h-56 lg:w-64 lg:h-64 mx-auto my-4 rounded-full"></div>
+            <div class="bg-indigo-800 w-56 h-56 lg:w-64 lg:h-64 mx-auto my-4 rounded-full"></div>
+        </div>
+    </section>
     </div>
 </template>
