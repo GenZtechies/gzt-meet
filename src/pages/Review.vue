fabc026feb02c93ef97ba20b7896c401d8ea7049
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-02-07 01:23:26 +0100

    fix: page overflow

--- src/pages/Review.vue
+++ src/pages/Review.vue
@@ -1,10 +1,11 @@
 <template>
     <main class="flex justify-center items-center w-full h-screen bg-gray-300 mt-[-5.3em]">
         <div class="relative z-100 bg-white w-4/5 max-w-lg opacity-100 p-3 rounded-md shadow">
-            <h1 class="text-xl text-center text-gray-900 font-medium leading-loose">This meeting has ended</h1>
+            <h1 class="text-2xl text-center text-gray-900 font-bold my-3">This meeting has ended</h1>
             <p class="text-center text-gray-800">Would you mind dropping a review? It would help us make the experience better for you.</p>
-            <div>
-                <button><router-link to="/">No please, take me </router-link></button>
+            <div class="flex flex-col space-y-2 mt-4">
+                <button class="bg-green-600 hover:bg-green-700 focus:ring-2 focus:ring-offset-2 focus:ring-green-200 w-full p-2 rounded-md text-white font-medium"><router-link to="">Submit Review</router-link></button>
+                <button class="border w-full p-2 rounded-md text-gray-800 font-medium hover:bg-gray-50 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"><router-link to="/">No thanks, please take me home</router-link></button>
             </div>
         </div>
     </main>
