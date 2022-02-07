5c7ae525a81bed56b1bc43588d59e4c82b4a7260
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-02-07 01:22:55 +0100

    added review page

---
+++ src/pages/Review.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="flex justify-center items-center w-full h-screen bg-gray-300 mt-[-5.3em]">
+        <div class="relative z-100 bg-white w-4/5 max-w-lg opacity-100 p-3 rounded-md shadow">
+            <h1 class="text-xl text-center text-gray-900 font-medium leading-loose">This meeting has ended</h1>
+            <p class="text-center text-gray-800">Would you mind dropping a review? It would help us make the experience better for you.</p>
+            <div>
+                <button><router-link to="/">No please, take me </router-link></button>
+            </div>
+        </div>
+    </main>
+</template>
